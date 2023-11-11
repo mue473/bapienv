108add98c5832c81722383a4de82a2060a000afe
mue473 <rainer@mueller-korntal.de>
2023-11-11 18:39:18 +0100

    add some files for startup

---
+++ armbsetup/installfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# This file is free software, C 2018 - 2023 Rainer Müller
+
+# General
+cp -p files/rc.local /etc/
+chmod +x /etc/rc.local
+
+cp -p files/temp-blacklist.conf /etc/modprobe.d/
+cp -p files/canhandling /etc/init.d/
+
+# HTTP Server
+cp -p files/lighttpd.conf /etc/lighttpd/
+cp -p files/10-cgi.conf /etc/lighttpd/conf-enabled/
+
+# prepare service handling
+cp -p files/canhandling /etc/init.d/
+chmod +x /etc/init.d/canhandling
+
+systemctl daemon-reload
+systemctl enable canhandling
+
+# allow daemon to write into /usr/local/etc directory
+chgrp daemon /usr/local/etc
+chmod g+w /usr/local/etc
+
+# enable user to shut down
+chmod 4711 /bin/systemctl
+
+# update initramfs
+update-initramfs -u
+
