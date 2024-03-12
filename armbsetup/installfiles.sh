50878a1a830fd957328e601e283366ab5ac01659
mue473 <rainer@mueller-korntal.de>
2024-03-12 15:22:42 +0100

    simplify setup of new environment

--- armbsetup/installfiles.sh
+++ armbsetup/installfiles.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# This file is free software, C 2018 - 2023 Rainer Müller
+# This file is free software, C 2018 - 2024 Rainer Müller
 
 # General
 cp -p files/rc.local /etc/
 chmod +x /etc/rc.local
 
 cp -p files/temp-blacklist.conf /etc/modprobe.d/
+cp -p files/i2card.conf /etc/modprobe.d/
 cp -p files/canhandling /etc/init.d/
 
 # HTTP Server
 cp -p files/lighttpd.conf /etc/lighttpd/
 cp -p files/10-cgi.conf /etc/lighttpd/conf-enabled/
-
+cp -rp ../www/ /
+ 
 # prepare service handling
 cp -p files/canhandling /etc/init.d/
 chmod +x /etc/init.d/canhandling
@@ -28,4 +30,3 @@ chmod 4711 /bin/systemctl
 
 # update initramfs
 update-initramfs -u
-
